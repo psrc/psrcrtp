b6519ae84ce781fbafcde8fd0d7432fc67cd56ca
Craig Helmann <chelmann@psrc.org>
2023-01-20 07:12:04 -0800

    gtfs stops by node

--- R/gtfs_data.R
+++ R/gtfs_data.R
@@ -10,11 +10,13 @@
 #' @importFrom rlang .data
 #' 
 #' @examples
-#' routes_2015 <- process_gtfs_route_data(year=2015, service_change = "Spring")
+#' routes_2015 <- transit_routes(year=2015, service_change = "Spring")
 #' 
 #' @export
 #'
-process_gtfs_route_data <- function(year, service_change) {
+transit_routes <- function(year, service_change) {
+  
+  if (tolower(service_change)=="spring") {data_month = "04"} else (data_month = "10")
   
   # Silence the dplyr summarize message
   options(dplyr.summarise.inform = FALSE)
@@ -66,7 +68,72 @@ process_gtfs_route_data <- function(year, service_change) {
       route_type == 3 ~ "Bus",
       route_type == 4 ~ "Ferry")) %>%
     # Remove Extra Attributes
-    dplyr::select("route_id", "route_short_name", "route_long_name", "agency_name", "transit_type")
+    dplyr::select("route_id", "route_short_name", "route_long_name", "agency_name", "transit_type") %>%
+    # Add Date to results
+    dplyr::mutate(date=lubridate::mdy(paste0(data_month,"-01-",year)))
+  
   print("All done.")
+  
   return(routes)
+}
+
+#' GTFS Stops by Mode
+#'
+#' This function pulls and cleans GTFS Stop data from the PSRC server for use in further transit analysis.
+#' 
+#' @param year Year of gtfs data as a four digit integer - available from 2015 to current year
+#' @param service_change Service Change period - either Fall or Spring 
+#' @return tibble of gtfs stop level data by service period year and mode
+#' 
+#' @importFrom magrittr %<>% %>%
+#' @importFrom rlang .data
+#' 
+#' @examples
+#' stops_2021 <- transit_stops_by_mode(year=2021, service_change = "Spring")
+#' 
+#' @export
+#'
+transit_stops_by_mode <- function(year, service_change) {
+  
+  if (tolower(service_change)=="spring") {data_month = "04"} else (data_month = "10")
+  
+  options(dplyr.summarise.inform = FALSE)
+  gtfs_file <- paste0("X:DSA/shiny-uploads/gtfs/",tolower(service_change),"_",as.character(year),".zip")
+  
+  # Get Route Details to get modes on the routes
+  routes <- psrcrtp::transit_routes(year, service_change) %>%
+    dplyr::select("route_id", "agency_name", "transit_type")
+  
+  # Open Regional GTFS File for stop times
+  print(paste0("Opening the ",service_change, " ", year, " GTFS archive to get Stop Times to get unique stops by route."))
+  gtfs <- tidytransit::read_gtfs(path=gtfs_file, files = c("trips","stops","stop_times"))
+  
+  # Clean Up Stop Times to get routes and mode by stops served
+  print(paste0("Cleaning the ",service_change, " ", year, " GTFS Stop Times to capture stps by Agency and Mode."))
+  
+  stoptimes <- tidyr::as_tibble(gtfs$stop_times) %>%
+    dplyr::select("trip_id", "stop_id")
+  
+  # Need to use trips to get route id for use in stop times
+  trips <- tidyr::as_tibble(gtfs$trips) %>%
+    dplyr::select("trip_id", "route_id")
+  
+  trips <- dplyr::left_join(trips, routes, by=c("route_id"))
+  
+  # Get Coordinates from Stops
+  stops <- tidyr::as_tibble(gtfs$stops) %>%
+    dplyr::select("stop_id", "stop_name", "stop_lat", "stop_lon")
+  
+  # Get Mode and agency from trips to stops
+  print(paste0("Getting unqiue stop list by modes for the ",service_change, " ", year, " GTFS archive."))
+  final_stops <- dplyr::left_join(stoptimes, trips, by=c("trip_id")) %>%
+    dplyr::select(-"trip_id", -"route_id") %>%
+    dplyr::distinct()
+  
+  final_stops <- dplyr::left_join(final_stops, stops, by=c("stop_id")) %>%
+    dplyr::mutate(date=lubridate::mdy(paste0(data_month,"-01-",year)))
+  
+  print("All Done.")
+  
+  return(final_stops)
 }
