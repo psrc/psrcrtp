b1a584081d100e42264ba3c108a1a98d47c12945
Craig Helmann <chelmann@psrc.org>
2023-02-02 13:19:35 -0800

    added function for forecast jobs data

--- R/jobs_data.R
+++ R/jobs_data.R
@@ -46,3 +46,69 @@ employment_near_hct <- function(start_year = 2010) {
   return(processed)
   
 }
+
+#' Total Observed and Forecast Employment Growth
+#'
+#' This function pulls total employment estimates from PSRC's employment database in Elmer and the Macroforecast.
+#' The jobs estimates are already summarized by year in a table in Elmer.
+#' 
+#' @param forecast_base_yr Four digit integer for Base Year in Forecast Data - defaults to 2018 
+#' @return tibble of total jobs in the region by calendar year
+#' 
+#' @importFrom magrittr %<>% %>%
+#' @importFrom rlang .data
+#' 
+#' @examples 
+#' forecast_jobs <- jobs_forecast_data()
+#'  
+#' @export
+#'
+jobs_forecast_data <- function(forecast_base_yr=2018){
+  
+  # Silence the dplyr summarize message
+  options(dplyr.summarise.inform = FALSE)
+  
+  print("Getting Histroic Jobs Data")
+  oj <- psrcrtp::employment_near_hct() %>%
+    dplyr::filter(.data$variable=="Region") %>%
+    dplyr::mutate(grouping="Total Employment", geography_type="PSRC Region", share=1, variable="Total", metric="Observed Employment")
+  
+  # Annual Observed Employment Change
+  oc <- oj %>%
+    dplyr::group_by(.data$geography) %>%
+    dplyr::mutate(estimate = (.data$estimate-dplyr::lag(.data$estimate)), variable="Change") %>% 
+    tidyr::drop_na()
+  
+  o <- dplyr::bind_rows(oj, oc)
+  rm(oj,oc)
+  
+  print("Getting Jobs Forecast Data")
+  # Observed Data up to Forecast Base Year
+  fo <- o %>%
+    dplyr::mutate(metric="Forecast Employment") %>%
+    dplyr::filter(lubridate::year(.data$date) <= forecast_base_yr)
+  
+  # Get Forecast Population Growth from Elmer
+  fj <- psrctrends::get_elmer_table("Macroeconomic.employment_facts") %>%
+    dplyr::filter(.data$employment_sector_dim_id==8 & .data$data_year >= forecast_base_yr) %>%
+    dplyr::rename(estimate="jobs") %>%
+    dplyr::mutate(date = lubridate::ymd(paste0(.data$data_year,"-04-01"))) %>%
+    dplyr::mutate(metric="Forecast Employment") %>% 
+    dplyr::select("date", "estimate", "metric") %>%
+    dplyr::mutate(geography="Region", grouping="Total Employment", geography_type="PSRC Region", share=1, variable="Total") 
+  
+  # Annual Forecast Employment Change
+  fc <- fj %>%
+    dplyr::group_by(.data$geography) %>%
+    dplyr::mutate(estimate = (.data$estimate-dplyr::lag(.data$estimate)), variable="Change") %>% 
+    tidyr::drop_na()
+  
+  fj <- fj %>%
+    dplyr::filter(lubridate::year(.data$date) > forecast_base_yr)
+  
+  f <- dplyr::bind_rows(o, fo, fj, fc)
+  rm(o, fo, fj, fc)
+  
+  print("All Done")
+  return(f)
+}
