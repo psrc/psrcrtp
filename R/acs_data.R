582c6e8cdafe7e83f4f05ea805baf41b2598067a
Craig Helmann <chelmann@psrc.org>
2023-02-23 15:26:44 -0800

    acs mode to work for cities in psrc region

--- R/acs_data.R
+++ R/acs_data.R
@@ -63,3 +63,68 @@ acs_mode_to_work_county <- function(years) {
   
   return(mode_data)
 }
+
+#' PSRC Cities ACS Work Mode Share
+#'
+#' This function pulls and cleans ACS Mode Share Data from Census Table B08006.
+#' Pulls data by year for cities in the PSRC Region
+#' 
+#' @param years Four digit integer (or vector of integers) for year(s) of analysis
+#' @return tibble of mode to work data by county and region by census year
+#' 
+#' @importFrom magrittr %<>% %>%
+#' @importFrom rlang .data
+#' 
+#' @examples
+#' modes_psrc <- acs_mode_to_work_place(years <- 2020)
+#' 
+#' @export
+#'
+acs_mode_to_work_place <- function(years) {
+  
+  commute_trips <- c("B08006_001")
+  da_trips <- c("B08006_003")
+  carpool_trips <- c("B08006_004")
+  transit_trips <- c("B08006_008")
+  bike_trips <- c("B08006_014")
+  walk_trips <- c("B08006_015")
+  other_trips <- c("B08006_016")
+  wfh_trips <- c("B08006_017")
+  
+  all_trips <- c(commute_trips, da_trips, carpool_trips, transit_trips, bike_trips, walk_trips, other_trips, wfh_trips)
+  non_da_trips <- c(carpool_trips,transit_trips,bike_trips,walk_trips,wfh_trips)
+  
+  mode_data = NULL
+  for (y in years) {
+    print(paste0("Working on ",y))
+    
+    modes <- psrccensus::get_acs_recs(geography = 'place',table.names = c('B08006'), years=y, acs.type = 'acs5') %>% 
+      dplyr::filter(.data$variable %in% all_trips) %>%
+      dplyr::filter(.data$census_geography == "City")
+    
+    totals <- modes %>% 
+      dplyr::filter(.data$variable %in% commute_trips) %>% 
+      dplyr::select("name", "year", "estimate") %>% 
+      dplyr::rename(total="estimate")
+    
+    modes <- dplyr::left_join(modes, totals, by=c("name","year")) %>% 
+      dplyr::mutate(share=.data$estimate/.data$total) %>%
+      dplyr::select("name", "estimate", "share", "moe", "label", "concept", "census_geography", "year") %>%
+      dplyr::mutate(label = stringr::str_replace_all(.data$label, "!!", " ")) %>%
+      dplyr::mutate(label = stringr::str_remove_all(.data$label, "Estimate Total: ")) %>%
+      dplyr::mutate(label = stringr::str_remove_all(.data$label, "Estimate Total ")) %>%
+      dplyr::mutate(label = stringr::str_remove_all(.data$label, "Car, truck, or van: ")) %>%
+      dplyr::mutate(label = stringr::str_remove_all(.data$label, "Car, truck, or van ")) %>%
+      dplyr::mutate(label = stringr::str_remove_all(.data$label, "Estimate ")) %>%
+      dplyr::mutate(label = stringr::str_remove_all(.data$label, ":")) %>%
+      dplyr::mutate(label = stringr::str_remove_all(.data$label, " \\(excluding taxicab\\)")) %>%
+      dplyr::rename(geography="name", variable="label", metric="concept", geography_type="census_geography") %>%
+      dplyr::mutate(date=lubridate::mdy(paste0("12-01-",.data$year)), grouping="PSRC Cities", share_moe=0, metric="Mode to Work") %>%
+      dplyr::select(-"year")
+    
+    if(is.null(mode_data)) {mode_data <- modes} else {mode_data <- dplyr::bind_rows(mode_data, modes)}
+    
+  }
+  
+  return(mode_data)
+}
