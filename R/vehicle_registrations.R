80fdb9afc72484c60e501693d998b6613bdf91d9
Craig Helmann <chelmann@psrc.org>
2023-10-23 10:28:49 -0700

    total ev registrations by county

--- R/vehicle_registrations.R
+++ R/vehicle_registrations.R
@@ -1,3 +1,73 @@
+#' Total Vehicle Registrations by Electrification Level and County
+#'
+#' This function pulls and cleans data from the Washington State Registration Data.
+#' Data is pulled from https://data.wa.gov/ via the Socrata API
+#' 
+#' @return tibble of total vehicle registrations by electrification level by month and county
+#' 
+#' @importFrom rlang .data
+#' 
+#' @examples
+#' \dontrun{
+#' total_vehicle_registrations <- ev_registrations_by_county()}
+#' 
+#' @export
+#'
+ev_registrations_by_county <- function () {
+  
+  # Silence the dplyr summarize message
+  options(dplyr.summarise.inform = FALSE)
+  
+  psrc_token <- "tjnJfQzL0SfZJ1cbT0iiCUpO3"
+  df <- dplyr::as_tibble(RSocrata::read.socrata("https://data.wa.gov/resource/3d5d-sdqb.json", app_token = psrc_token)) |>
+    dplyr::filter(.data$state %in% c("WA") & .data$county %in% c("King", "Kitsap", "Pierce", "Snohomish")) |>
+    dplyr::select(-"state", -"vehicle_primary_use", -"percent_electric_vehicles") |>
+    dplyr::rename(bev="battery_electric_vehicles_bevs_") |>
+    dplyr::rename(phev="plug_in_hybrid_electric_vehicles_phevs_") |>
+    dplyr::rename(ev="electric_vehicle_ev_total") |>
+    dplyr::rename(non_ev = "non_electric_vehicles") |>
+    dplyr::rename(geography = "county") |>
+    dplyr::mutate(bev=as.numeric(.data$bev), phev=as.numeric(.data$phev)) |>
+    dplyr::mutate(ev=as.numeric(.data$ev), non_ev=as.numeric(.data$non_ev)) |>
+    dplyr::mutate(total_vehicles=as.numeric(.data$total_vehicles)) |>
+    dplyr::group_by(.data$date, .data$geography) |>
+    dplyr::summarise(bev=sum(.data$bev), phev=sum(.data$phev), ev=sum(.data$ev), non_ev=sum(.data$non_ev), total_vehicles=sum(.data$total_vehicles)) |>
+    dplyr::as_tibble() 
+  
+  region <- df |>
+    dplyr::group_by(.data$date) |>
+    dplyr::summarise(bev=sum(.data$bev), phev=sum(.data$phev), ev=sum(.data$ev), non_ev=sum(.data$non_ev), total_vehicles=sum(.data$total_vehicles)) |>
+    dplyr::as_tibble() |>
+    dplyr::mutate(geography = "Region")
+  
+  df <- dplyr::bind_rows(df, region)
+  
+  share <- df |>
+    dplyr::mutate(bev_share = .data$bev / .data$total_vehicles) |>
+    dplyr::mutate(phev_share = .data$phev / .data$total_vehicles) |>
+    dplyr::mutate(ev_share = .data$ev / .data$total_vehicles) |>
+    dplyr::mutate(non_ev_share = .data$non_ev / .data$total_vehicles) |>
+    dplyr::mutate(total_vehicles_share = 1) |>
+    dplyr::select(-"bev", -"phev", -"ev", -"non_ev", -"total_vehicles") |>
+    tidyr::pivot_longer(!c("date", "geography"), names_to = "variable", values_to = "share") |>
+    dplyr::mutate(variable = stringr::str_remove_all(.data$variable, "_share"))
+  
+  totals <- df |>
+    tidyr::pivot_longer(!c("date", "geography"), names_to = "variable", values_to = "estimate")
+  
+  final_data <- dplyr::left_join(totals, share, by=c("date", "geography", "variable")) |>
+    dplyr::mutate(geography_type = "County", grouping = "All", metric = "total-vehicle-registrations") |>
+    dplyr::mutate(variable = stringr::str_replace_all(.data$variable, "total_vehicles", "Total Vehicles")) |>
+    dplyr::mutate(variable = stringr::str_replace_all(.data$variable, "bev", "Battery Electric")) |>
+    dplyr::mutate(variable = stringr::str_replace_all(.data$variable, "phev", "Plug-In Hybrid")) |>
+    dplyr::mutate(variable = stringr::str_replace_all(.data$variable, "non_ev", "Internal Combustion")) |>
+    dplyr::mutate(variable = stringr::str_replace_all(.data$variable, "ev", "Total Electric Vehicles")) |>
+    dplyr::select("date", "geography", "variable", "geography_type", "grouping", "metric", "estimate", "share")
+  
+  return(final_data)
+  
+}
+
 #' New Vehicle Registrations by Electrification Level
 #'
 #' This function pulls and cleans data from the Washington State Registration Data.
