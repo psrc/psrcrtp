b475adc58332fb197c01aa339c8d20e9da2dab97
Craig Helmann <chelmann@psrc.org>
2023-11-20 09:24:22 -0800

    map data for congestion

--- R/npmrds_data.R
+++ R/npmrds_data.R
@@ -1,7 +1,7 @@
 #' Congested Lane Miles by Time of Day and County
 #'
 #' This function cleans data from the NPMRDS output files from INRIX.
-#' Data is pre-processed through a python sccript on data downloaded from the RTIS site
+#' Data is pre-processed through a python script on data downloaded from the RTIS site
 #' 
 #' @param file_path Absolute file path to pre-processed travel time files from NPMRDS
 #' 
@@ -90,5 +90,60 @@ process_npmrds_data <- function(file_path = "X:/DSA/rtp-dashboard/NPMRDS/") {
     dplyr::select("year", "date", "geography", "geography_type", "variable", "grouping", "metric", "estimate", "share")
   
   return(c)
+  
+}
+
+#' Am, Midday and PM Speed Ratios for mapping
+#'
+#' This function cleans data from the NPMRDS output files from INRIX.
+#' Data is pre-processed through a python script on data downloaded from the RTIS site
+#' 
+#' @param file_path Absolute file path to pre-processed travel time files from NPMRDS
+#' 
+#' @return tibble am, midday and pm speed ratios by TMC with geometry for mapping
+#' 
+#' @importFrom rlang .data
+#' 
+#' @examples
+#' \dontrun{
+#' congestion_map_data <- map_npmrds_data()}
+#' 
+#' @export
+#'
+map_npmrds_data <- function(file_path = "X:/DSA/rtp-dashboard/NPMRDS/") {
+  
+  wgs84 <- 4326
+  
+  print(stringr::str_glue("Opening travel time shapefile"))
+  tmc <- sf::st_read(paste0(file_path,"shapefiles/", as.character(lubridate::year(Sys.Date())), "/Washington.shp")) |>
+    dplyr::select("Tmc", roadway="RoadName", geography="County") |>
+    dplyr::filter(.data$geography %in% c("KING", "KITSAP","PIERCE", "SNOHOMISH")) |>
+    dplyr::mutate(geography = stringr::str_to_title(.data$geography)) |>
+    sf::st_transform(wgs84) |>
+    tidyr::drop_na()
+  
+  processed <- NULL
+  for (file in list.files(path = file_path, pattern = ".*.csv")) {
+    d <- stringr::str_remove_all(file, "_cars_tmc_95th_percentile_speed_weekdays.csv")
+    print(stringr::str_glue("Opening travel time data for weekdays in {d}"))
+    
+    t <- readr::read_csv(paste0(file_path, file), show_col_types = FALSE) |> 
+      dplyr::mutate(date=d) |>
+      dplyr::mutate(lane_miles = .data$miles * .data$thrulanes_unidir) |>
+      tidyr::separate(col=.data$date, into = c("date","year")) |>
+      dplyr::mutate(year = as.character(.data$year)) |>
+      dplyr::mutate(date = lubridate::mdy(paste0(.data$date,"-01-",.data$year))) |>
+      dplyr::filter(!(is.na(.data$f_system))) |>
+      dplyr::select("Tmc", "date", "year", am_peak="7am_ratio", midday="Noon_ratio", pm_peak="4pm_ratio") |>
+      tidyr::drop_na()
+    
+    print(stringr::str_glue("Joining speed data to shapefile for {d}"))
+    p <- dplyr::left_join(tmc, t, by="Tmc") |> tidyr::drop_na()
+    
+    ifelse(is.null(processed), processed <- p, processed <- dplyr::bind_rows(processed, p))
+    
+  }
+  
+  return(processed)
   
 }
